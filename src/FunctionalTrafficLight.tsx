6b0674fe84c514e28878d970bc9e91514534d77e
mnn jhn <michaelmontero9319@gmail.com>
2023-06-30 00:10:13 -0400

    Initial commit
    modify the functional component

--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -1,17 +1,48 @@
+import { useState } from "react";
+
+const Lightoff = "circle black";
+const trafficLight = ["lets go!", "slow down", "stop"];
+
 export const FunctionalTrafficLight = () => {
+  const [lightIndex, setLightIndex] = useState(0);
+  const currentLight = trafficLight[lightIndex];
+  const lastLight = lightIndex === trafficLight.length - 1; // last in the array
+
+  const nextLight = () => {
+    lastLight ? setLightIndex(lightIndex - 2) : setLightIndex(lightIndex + 1);
+  };
+
   return (
-    <div className="traffic-light-box">
-      <h2>Functional Traffic Light</h2>
-      <div className="traffic-light">
-        {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+    <>
+      <div className="traffic-light-box">
+        <h2>Functional Traffic Light</h2>
+        <div className="traffic-light">
+          {/* Background color can be black | yellow | red | green */}
+          <div
+            className={
+              currentLight === trafficLight[2] ? "circle red" : Lightoff
+            }
+          ></div>
+          <div
+            className={
+              currentLight === trafficLight[1] ? "circle yellow" : Lightoff
+            }
+          ></div>
+          <div
+            className={
+              currentLight === trafficLight[0] ? "circle green" : Lightoff
+            }
+          ></div>
+        </div>
+        <button
+          onClick={() => {
+            nextLight();
+          }}
+          className="next-state-button"
+        >
+          Next State
+        </button>
       </div>
-      <button className="next-state-button">Next State</button>
-    </div>
+    </>
   );
 };
-
-
-
